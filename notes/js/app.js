f5aa936c5d5ee3efbda4abd6b43cb3c93fa200a8
archilleu <313720180@qq.com>
2018-01-10 22:58:24 +0800

    add test

--- notes/js/app.js
+++ notes/js/app.js
@@ -1,21 +1,77 @@
-$(function(){
+$(function () {
 
-    var gdrive = new GDrive();
-    gdrive.auth(function(lastError){
-      if(null != lastError) {
-        console.log("auth success"+lastError);
+  const folderName = "test";
+  const fileName = "test.txt";
+  let curFolder = null;
+  let curFile = null;
+
+  const gdrive = new GDrive();
+  gdrive.auth(function (lastError) {
+    if (null != lastError) {
+      console.log("auth success" + lastError);
+      return;
+    }
+
+    gdrive.createFolder(null, "test", function (result) {
+      if (null != result.message) {
+        console.log(result.message);
         return;
       }
 
-      gdrive.createFolder(null, "test", function(result){
-        console.log(result);
+      gdrive.list(null, function (result) {
+        if (null != result.message) {
+          console.log(result.message);
+          return;
+        }
+
+        for (file in result.data.files) {
+          if (folderName == file.name) {
+            curFolder = file;
+            break;
+          }
+        }
+
+        if (null == curFolder) {
+          console.error("create failed!");
+          return;
+        }
+
+        gdrive.createFileMetadata(curFolder.id, "text.txt", function (result) {
+          if (null != result.message) {
+            console.log(result.message);
+            return;
+          }
+
+          gdrive.list(curFolder.id, function (result) {
+            if (null != result.message) {
+              console.log(result.message);
+              return;
+            }
+
+            for (file in result.data.files) {
+              if (fileName == file.name) {
+                curFile = file;
+                break;
+              }
+            }
+
+            gdrive.createFileContent(result.data.id, "abcd", function (result) {
+              if (null != result.message) {
+                console.log(result.message);
+                return;
+              }
+            });
+
+          });
+        });
       });
+    });
     //   gdrive.removeCachedAuth(function(){
     //     gdrive.auth(function(){
     //       gdrive.revokeAuth();
     //     })
     //   })
-    })
+  })
 
-    return;
+  return;
 });
--- notes/js/gdrive.js
+++ notes/js/gdrive.js
@@ -7,50 +7,62 @@
 function GDrive() {
   this.accessToken = null;
 
-  this.__defineGetter__("DEFAULT_CHUNK_SIZE", function() {
+  this.__defineGetter__("DEFAULT_CHUNK_SIZE", function () {
     return 1024 * 1024 * 5; // 5MB;
   });
 
-  this.__defineGetter__("REST_FOLDER_CREATE", function() {
+  this.__defineGetter__("REST_FOLDER_CREATE", function () {
     return "https://www.googleapis.com/drive/v3/files";
   })
 
-  this.__defineGetter__("REST_FILE_CREATE_METADATA", function() {
+  this.__defineGetter__("REST_FOLDER_DELETE", function () {
+    return "https://www.googleapis.com/drive/v3/files/"/*fileId*/;
+  })
+
+  this.__defineGetter__("REST_FOLDER_LIST", function () {
+    return "https://www.googleapis.com/drive/v3/files?corpus=user";
+  })
+
+  this.__defineGetter__("REST_FILE_CREATE_METADATA", function () {
     return "https://www.googleapis.com/drive/v3/files";
   })
 
-  this.__defineGetter__("REST_FILE_CONTENT", function() {
-    return "https://www.googleapis.com/upload/drive/v3/files/" /*fileid +?uploadType=media or ...*/;
+  this.__defineGetter__("REST_FILE_DELETE", function () {
+    return "https://www.googleapis.com/drive/v3/files/"/*fileId*/;
   })
 
-  this.__defineGetter__("REST_FILE_CONTENT_UPLOADMEDIA", function() {
+  this.__defineGetter__("REST_FILE_CONTENT", function () {
+    return "https://www.googleapis.com/upload/drive/v3/files/"/*fileid +?uploadType=media or ...*/;
+  })
+
+  this.__defineGetter__("REST_FILE_CONTENT_UPLOADMEDIA", function () {
     return "?uploadType=media";
   })
 
 }
 
 //Authorization
-GDrive.prototype.auth = function(callback) {
+GDrive.prototype.auth = function (callback) {
   try {
-      chrome.identity.getAuthToken({interactive: true}, function(token) {
-        if(chrome.runtime.lastError) {
-          callback && callback(chrome.runtime.lastError.message);
-          return;
-        }
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      if (chrome.runtime.lastError) {
+        callback && callback(chrome.runtime.lastError.message);
+        return;
+      }
 
-        if(token) {
-          this.accessToken = token;
-          callback && callback(null);
-        }
+      if (token) {
+        this.accessToken = token;
+        callback && callback(null);
+      }
     }.bind(this));
-  } catch(e) {
+  } catch (e) {
     console.error(e);
   }
 }
 
 //remove cache auth
-GDrive.prototype.removeCachedAuth = function(callback) {
-  if(null == this.accessToken) {
+GDrive.prototype.removeCachedAuth = function (callback) {
+  if (null == this.accessToken) {
     callback && callback();
     return;
   }
@@ -58,30 +70,30 @@ GDrive.prototype.removeCachedAuth = function(callback) {
   const accessToken = this.accessToken;
   this.accessToken = null;
 
-  chrome.identity.removeCachedAuthToken({token: accessToken},
-    function() {
+  chrome.identity.removeCachedAuthToken({ token: accessToken },
+    function () {
       callback && callback();
     }
   );
 }
 
 //revoke auth
-GDrive.prototype.revokeAuth = function() {
-    if(this.accessToken) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' + this.accessToken);
-        xhr.send();
-        this.removeCachedAuth();
-    }
+GDrive.prototype.revokeAuth = function () {
+  if (this.accessToken) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' + this.accessToken);
+    xhr.send();
+    this.removeCachedAuth();
+  }
 }
 
 //create folder
-GDrive.prototype.createFolder = function(parent, name, callback) {
-  if(null == this.accessToken) {
+GDrive.prototype.createFolder = function (parent, name, callback) {
+  if (null == this.accessToken) {
     callback && callback({
-        message:"token is null",
-        data: null
-      });
+      message: "token is null",
+      data: null
+    });
     return;
   }
 
@@ -95,21 +107,79 @@ GDrive.prototype.createFolder = function(parent, name, callback) {
     url: this.REST_FOLDER_CREATE,
     contentType: 'application/json',
     data: JSON.stringify(metadata),
-    beforeSend: function(request) {
+    beforeSend: function (request) {
       request.setRequestHeader("Authorization", 'Bearer ' + this.accessToken);
     }.bind(this),
-    success: function(data){
+    success: function (data) {
       callback && callback({
         message: null,
         data: data
+      });
+    },
+    error: function (jqXHR, textStatus, errorTrown) {
+      callback && callback({
+        message: textStatus,
+        errorTrown: errorTrown,
+        jqXHR: jqXHR
+      });
+    }
+  });
+}
+
+//delete folder
+GDrive.prototype.deleteFolder = function (folderId, callback) {
+  $.ajax({
+    type: "DELETE",
+    url: this.REST_FOLDER_DELETE + folderId,
+    contentType: 'application/json',
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", 'Bearer ' + this.accessToken);
+    }.bind(this),
+    success: function (data) {
+      callback && callback({
+        message: null,
+        data: data
+      });
+    },
+    error: function (jqXHR, textStatus, errorTrown) {
+      callback && callback({
+        message: textStatus,
+        errorTrown: errorTrown,
+        jqXHR: jqXHR
+      });
+    }
+  });
+}
+
+//list
+GDrive.prototype.list = function (parentId, callback) {
+  const param = this.REST_FOLDER_LIST + (parentId ? "?q=" + parentId : "");
+  $.ajax({
+    type: "GET",
+    url: param,
+    contentType: 'application/json',
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", 'Bearer ' + this.accessToken);
+    }.bind(this),
+    success: function (data) {
+      callback && callback({
+        message: null,
+        data: data
+      });
+    },
+    error: function (jqXHR, textStatus, errorTrown) {
+      callback && callback({
+        message: textStatus,
+        errorTrown: errorTrown,
+        jqXHR: jqXHR
       });
     }
   });
 }
 
 //create file fileMetadata
-GDrive.prototype = createFileMetadata(parent, name, callback) {
-  const metadata= {
+GDrive.prototype.createFileMetadata = function (parent, name, callback) {
+  const metadata = {
     name: name,
     parents: parent ? [parent] : null
   }
@@ -118,32 +188,71 @@ GDrive.prototype = createFileMetadata(parent, name, callback) {
     url: this.REST_FILE_CREATE_METADATA,
     contentType: 'application/json',
     data: JSON.stringify(metadata),
-    beforeSend: function(request) {
+    beforeSend: function (request) {
       request.setRequestHeader("Authorization", 'Bearer ' + this.accessToken);
     }.bind(this),
-    success: function(data){
+    success: function (data) {
       callback && callback({
         message: null,
         data: data
+      });
+    },
+    error: function (jqXHR, textStatus, errorTrown) {
+      callback && callback({
+        message: textStatus,
+        errorTrown: errorTrown,
+        jqXHR: jqXHR
+      });
+    }
+  });
+}
+
+//delete file 
+GDrive.prototype.deleteFile = function (fileId) {
+  $.ajax({
+    type: "DELETE",
+    url: this.REST_FILE_DELETE + fileId,
+    contentType: 'application/json',
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", 'Bearer ' + this.accessToken);
+    }.bind(this),
+    success: function (data) {
+      callback && callback({
+        message: null,
+        data: data
+      });
+    },
+    error: function (jqXHR, textStatus, errorTrown) {
+      callback && callback({
+        message: textStatus,
+        errorTrown: errorTrown,
+        jqXHR: jqXHR
       });
     }
   });
 }
 
 //create file fileMetadata
-GDrive.prototype = createFileContent(fileId, data, callback) {
+GDrive.prototype.createFileContent = function (fileId, data, callback) {
   $.ajax({
     type: "PATCH",
     url: this.REST_FILE_CONTENT + fileId + this.REST_FILE_CONTENT_UPLOADMEDIA,
     contentType: 'application/json',
     data: data,
-    beforeSend: function(request) {
+    beforeSend: function (request) {
       request.setRequestHeader("Authorization", 'Bearer ' + this.accessToken);
     }.bind(this),
-    success: function(data){
+    success: function (data) {
       callback && callback({
         message: null,
         data: data
+      });
+    },
+    error: function (jqXHR, textStatus, errorTrown) {
+      callback && callback({
+        message: textStatus,
+        errorTrown: errorTrown,
+        jqXHR: jqXHR
       });
     }
   });

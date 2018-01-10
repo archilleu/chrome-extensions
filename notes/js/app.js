f70e3161240e06ef53115e8608217df40b99cf24
archilleu <313720180@qq.com>
2018-01-10 17:33:32 +0800

    add test

--- notes/js/app.js
+++ notes/js/app.js
@@ -1,29 +1,21 @@
 $(function(){
 
-    chrome.identity.onSignInChanged.addListener(function(account, signedIn){
-        console.log("account:");
-        console.log(account);
-        console.log("signedIn");
-        console.log(signedIn);
-    });
-
-    // chrome.identity.getAuthToken({interactive:true/* details */},
-    chrome.identity.getAuthToken({/* details */}, function (access_token) {
-      if (chrome.runtime.lastError) {
-        callback(chrome.runtime.lastError);
+    var gdrive = new GDrive();
+    gdrive.auth(function(lastError){
+      if(null != lastError) {
+        console.log("auth success"+lastError);
         return;
       }
 
-    const url = "https://www.googleapis.com/drive/v3/files?pageSize=10";
+      gdrive.createFolder(null, "test", function(result){
+        console.log(result);
+      });
+    //   gdrive.removeCachedAuth(function(){
+    //     gdrive.auth(function(){
+    //       gdrive.revokeAuth();
+    //     })
+    //   })
+    })
 
-    $.ajax({
-      url: url,
-      beforeSend: function(request) {
-      request.setRequestHeader("Authorization", 'Bearer ' + access_token);
-      },
-      success: function(data){
-        console.log(data);
-      }
-    });
-  });
+    return;
 });

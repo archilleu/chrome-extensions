be7b2e70a03a1d4d66a2b94b8c96f2ad532bdbcb
archilleu <313720180@qq.com>
2018-01-17 17:19:48 +0800

    1.本来想自己实现个简单的文本编辑的， 可是键盘事件和光标移动，增改删等等 一系列的操作，崩溃了。。。，我认熊 2.作为替代使用CodeMirror简单的嵌入 即可完成我要的功能了

---
+++ notes/js/editor.js
@@ -0,0 +1,31 @@
+$(function() {
+  init();
+
+  function init() {
+    addEditorArea();
+    initEditor();
+  }
+
+  function bindBtnClickEvent() {}
+
+  function addEditorArea() {
+    $(".note-editor").append("<textarea id='note-editor' style='display: none;'></textarea>");
+  }
+
+  function initEditor() {
+    const editor = CodeMirror.fromTextArea(document.getElementById("note-editor"), {
+      lineWrapping: true,
+      tabSize: 4,
+      scrollbarStyle: "simple",
+      cursorHeight: 0.40,
+      viewportMargin: Infinity,
+      extraKeys: {
+        // "Esc": function() {
+        // }
+      }
+    });
+
+    window.editor = editor;
+  }
+
+});

0a9226feca601eea63fa3d322b72868db71b0a12
archilleu <archilleu9527@gmail.com>
2019-08-28 22:09:02 +0800

    修改登录页

--- notes/js/login.js
+++ notes/js/login.js
@@ -1,31 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const gdrive = chrome.extension.getBackgroundPage().gdrive;
+    const gdrive = chrome.extension.getBackgroundPage().gdrive;
 
-  if (gdrive.checkAuth()) {
-    gotoMainWindow();
-  }
+    const google = document.getElementById("google-login");
+    google.addEventListener("click", function(event) {
+        event.preventDefault();
 
-  const google = document.getElementById("google-login");
-  google.addEventListener("click", function(event) {
-    event.preventDefault();
+        gdrive.auth({
+            success: () => {
+                gotoMainWindow();
+            },
+            error: (msg) => {
+                console.log(msg);
+            }
+        });
+    }, false);
 
-    gdrive.auth({
-      success: () => {
-        gotoMainWindow();
-      },
-      error: (msg) => {
-        console.log(msg);
-      }
-    });
-  }, false);
+    const qq = document.getElementById("qq-login");
+    qq.addEventListener("click", function() {
+        event.preventDefault();
+        alert("QQ登陆未开放");
+    }, false);
 
-  const qq = document.getElementById("qq-login");
-  qq.addEventListener("click", function() {
-    event.preventDefault();
-  }, false);
-
-  function gotoMainWindow() {
-    window.location.href = chrome.extension.getURL('window.html');
-  }
+    function gotoMainWindow() {
+        window.location.href = chrome.extension.getURL('window.html');
+    }
 });

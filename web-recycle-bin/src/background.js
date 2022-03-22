381c13ad5cae4eda69c1cc96485393b028ce1d4a
chenjianyu <chenjianyu@example.com>
2022-03-22 16:36:10 +0800

    升级mainfest v3,去掉vue依赖

--- web-recycle-bin/src/background.js
+++ web-recycle-bin/src/background.js
@@ -1,10 +1,111 @@
+class Tabs {
+  constructor() {
+    this.openedTabs = new Map(); // 已经打开的标签，由于关闭标签事件只有一个tabId所以需要记录打开的标签页
+    this.closedTabs = new Array(); // 已经关闭的标签
+
+  }
+
+  getClosedTabs(start, end) {
+    if (start > end)
+      return;
+
+    start = (undefined == start) ? 0 : start;
+    end = (undefined == end) ? this.closedTabs.length : end;
+    return this.closedTabs.slice(start, end);
+  }
+
+  delClosedTab(tabId) {
+    for (var i = 0; i < this.closedTabs.length; i++) {
+      if (this.closedTabs[i].id == tabId) {
+        this.closedTabs.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  clearClosedTabs() {
+    this.closedTabs = new Array();
+  }
+
+  addOpenedTab(tab) {
+
+    this.openedTabs.set(tab.id, {
+      id: tab.id,
+      index: tab.index,
+      title: tab.title,
+      url: tab.url,
+      favicon: tab.favIconUrl || "../images/default.png"
+    });
+  }
+
+  addClosedTab(tabId) {
+    if (!this._isOpenedTab(tabId)) {
+      return;
+    }
+
+    let tab = this.openedTabs.get(tabId);
+    tab.date = new Date();
+    this.closedTabs.unshift(tab);
+
+    this.openedTabs.delete(tabId);
+  }
+
+  //检测是否是打开的标签页
+  _isOpenedTab(id) {
+    return this.openedTabs.has(id);
+  }
+};
+const tabs = new Tabs()
+
+const REG_LEGAL_URL = /^(http:|https:|ftp:|file:)/
+function checkLegalUrl(tab) {
+  if (tab.url && REG_LEGAL_URL.test(tab.url)) {
+    return true;
+  }
+
+  return false;
+}
+
+chrome.runtime.onMessage.addListener(
+  (request, sender, sendResponse) => {
+    if (request.getTabs) {
+      const page = request.getTabs
+      sendResponse({ tabs: tabs.getClosedTabs(page.start, page.end), total: tabs.closedTabs.length });
+    } else if (request.delTabs) {
+      tabs.delClosedTab(request.delTabs)
+      sendResponse({});
+    }  else if(request.clear) {
+      tabs.clearClosedTabs()
+      sendResponse({});
+    } else {
+      // TODO NOTHING
+    }
+
+  }
+);
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.status !== "complete") {
+    return;
+  }
+
+  if (!this.checkLegalUrl(tab)) {
+    return;
+  }
+
+  tabs.addOpenedTab(tab)
+});
+
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+  tabs.addClosedTab(tabId)
+});
+
 class TestTabs {
     assert(condition, msg) {
         if (!condition) {
             throw new Error(`[WRB] ${msg}`)
         }
     }
-
 
     test() {
         //测试空的closedTabs
@@ -75,10 +176,10 @@ class TestTabs {
         //删除closed tab
         const size = tabs.closedTabs.length;
         for (let i = 0; i < size - 1; i++) {
-            tabs.deleteClosedTab(i + tabIdIndex);
+            tabs.delClosedTab(i + tabIdIndex);
         }
-        this.assert(tabs.closedTabs.length == 1, "func deleteClosedTab failed!");
-        this.assert(tabs.getClosedTabs()[0].id == 1004, "func deleteClosedTab failed!");
+        this.assert(tabs.closedTabs.length == 1, "func delClosedTab failed!");
+        this.assert(tabs.getClosedTabs()[0].id == 1004, "func delClosedTab failed!");
 
         //清空closed tab
         tabs.clearClosedTabs();
@@ -87,7 +188,5 @@ class TestTabs {
 
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const testTabs = new TestTabs();
-    testTabs.test();
-});
+const testTabs = new TestTabs();
+testTabs.test();
---
+++ web-recycle-bin/src/popup.js
@@ -0,0 +1,140 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  let curPage = 1
+  let pageCount = 0
+  let totalPage = 0
+  let tabs = []
+  const home = 1
+  const pageSize = 2
+
+  const listDom = document.getElementById("item-list");
+  const prevDom = document.getElementById("prev")
+  const nextDom = document.getElementById("next")
+  const clearDom = document.getElementById("clear")
+  const curPageDom = document.getElementById("cur-page")
+  const totalPageDom = document.getElementById("total-page")
+
+  main()
+
+  function main() {
+    bindToolbarEvent()
+
+    reload()
+  }
+
+  function bindToolbarEvent() {
+    prevDom.addEventListener("click", () => {
+      if (home == curPage)
+        return;
+
+      curPage -= 1;
+      reload();
+    }, false)
+
+    nextDom.addEventListener("click", () => {
+      if (curPage == totalPage)
+        return;
+
+      curPage += 1;
+      reload();
+    }, false)
+
+    clearDom.addEventListener("click", () => {
+      chrome.runtime.sendMessage({ clear: 'clear' }, () => {
+        window.close();
+      });
+    }, false)
+  }
+
+  function reload() {
+    chrome.runtime.sendMessage({
+      getTabs: {
+        start: (curPage - 1) * pageSize,
+        end: curPage * pageSize
+      }
+    }, (request) => {
+      if (0 === request.tabs.length) {
+        return
+      }
+
+      tabs = request.tabs
+      pageCount = request.total
+      totalPage = Math.ceil(pageCount / pageSize);
+
+      updateToolbar()
+      removeAllItems()
+      createItemListHtml(tabs)
+    });
+  }
+
+  function updateToolbar() {
+    curPageDom.innerText = curPage
+    totalPageDom.innerText = totalPage
+    prevDom.style.display = (home === curPage) ? "none" : "inline-block"
+    nextDom.style.display = (curPage === totalPage) ? "none" : "inline-block"
+  }
+
+  function removeAllItems() {
+    while (listDom.firstChild) {
+      listDom.removeChild(listDom.firstChild);
+    }
+  }
+
+  function createItemListHtml(tabs) {
+    const itemList = document.getElementById("item-list")
+    tabs.forEach(tab => {
+      itemList.appendChild(createItem(tab))
+    });
+  }
+
+  function createItem(tab) {
+    const item = createDiv(tab)
+    item.appendChild(createFavicon(tab))
+    item.appendChild(createLink(tab))
+    item.appendChild(createTime(tab))
+    return item
+  }
+
+  function createDiv() {
+    const div = document.createElement("div");
+    div.setAttribute("class", "item");
+    div.dataset.id = tab.id;
+    div.dataset.index = tab.index;
+    div.addEventListener("click", function () {
+      const url = this.getElementsByTagName("a")[0].href;
+      const option = {
+        index: parseInt(this.dataset.index),
+        url: url
+      }
+
+      chrome.runtime.sendMessage({ delTabs: this.dataset.id }, () => {
+        chrome.tabs.create(option);
+      });
+    }, false);
+    return div;
+  }
+
+  function createFavicon(tab) {
+    const favicon = document.createElement("img");
+    favicon.src = tab.favicon;
+    favicon.alt = "";
+    favicon.width = 16;
+    favicon.height = 16;
+    return favicon;
+  }
+
+  function createLink(tab) {
+    const a = document.createElement("a");
+    a.href = tab.url;
+    a.title = tab.title;
+    a.innerText = tab.title
+    return a;
+  }
+
+  function createTime(tab) {
+    const span = document.createElement("span");
+    span.innerText = new Date(tab.date).toLocaleTimeString();
+    return span;
+  }
+})
+
